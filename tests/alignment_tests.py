1c2982d99a9fd87a9eea7d85464349ef0b00847d
Francesco Andreace <francesco.andreace@gmail.com>
2025-02-19 15:43:40 +0100

    Starting testing alignment functions

---
+++ tests/alignment_tests.py
@@ -0,0 +1,24 @@
+import unittest
+
+from assembler.aligner import verify_path_concordance, verify_sequence_agreement
+
+
+
+path1 = ">1>2>3>4>5>6"
+
+
+class TestAlginmentFunctions(unittest.TestCase):
+
+
+    def test_verify_path_concordance():
+        pass
+
+
+    def test_verify_sequence_agreement():
+        pass
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
